4f5ab15c2860b52008a6361edda01e820754bf0a
Kjetil Pedersen <kjetil.pedersen79@gmail.com>
2019-06-13 22:19:31 +0200

    refactored: made cache and remote class use non static methods, added common interface BanksCallable

--- src/main/java/io/bankbridge/Main.java
+++ src/main/java/io/bankbridge/Main.java
@@ -1,20 +1,21 @@
 package io.bankbridge;
-import static spark.Spark.get;
-import static spark.Spark.port;
 
 import io.bankbridge.handler.BanksCacheBased;
 import io.bankbridge.handler.BanksRemoteCalls;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		
-		port(8080);
+    public static void main(String[] args) throws Exception {
 
-		BanksCacheBased.init();
-		BanksRemoteCalls.init();
-		
-		get("/v1/banks/all", (request, response) -> BanksCacheBased.handle(request, response));
-		get("/v2/banks/all", (request, response) -> BanksRemoteCalls.handle(request, response));
-	}
+        port(8080);
+
+        BanksCacheBased.init();
+        BanksRemoteCalls.init();
+
+        get("/v1/banks/all", BanksCacheBased::handle); // simplify call
+        get("/v2/banks/all", BanksRemoteCalls::handle); // simplify call
+    }
 }
---
+++ src/main/java/io/bankbridge/handler/BanksCallable.java
@@ -0,0 +1,4 @@
+package io.bankbridge.handler;
+
+public interface BanksCallable {
+}
